0af61284426b4cbbb5e0a27ed4ad8f6245ea3f72
Mike Goldin <mike.goldin@protonmail.ch>
2019-03-02 01:53:08 +0000

    Get a port from the environment

--- src/main/java/sys/JoNet/Api.java
+++ src/main/java/sys/JoNet/Api.java
@@ -6,6 +6,7 @@ import sys.JoNet.auth.GenerateUserTokenRequestHandler;
 
 public class Api {
   public static void main(String[] args) {
+    port(Integer.parseInt(System.getenv("JONET_PORT")));
     post("/GenerateUserToken/", new GenerateUserTokenRequestHandler());
   }
 }
