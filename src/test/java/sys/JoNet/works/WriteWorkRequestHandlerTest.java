2c6d0b00f2abd1dae923d1f078eeebd04e8307dd
Mike Goldin <mike.goldin@protonmail.ch>
2019-03-05 21:13:43 -0500

    Stub in WriteWork handler tests

---
+++ src/test/java/sys/JoNet/works/WriteWorkRequestHandlerTest.java
@@ -0,0 +1,18 @@
+package sys.JoNet.works;
+
+import org.junit.jupiter.api.*;
+
+class WriteWorkRequestHandlerTest {
+
+  @Test
+  @Disabled
+  void shouldWriteANewWork() {
+
+  }
+
+  @Test
+  @Disabled
+  void shouldNotOverwriteAnExtantWork() {
+
+  }
+}
