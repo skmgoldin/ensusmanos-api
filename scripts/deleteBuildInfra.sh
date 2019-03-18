ca669da9599b9ff6a8f9b748af4a58a963cc88ef
Mike Goldin <mike.goldin@protonmail.ch>
2019-03-18 01:30:43 -0400

    Fix bug in deleteBuildInfra script
    By not waiting to delete the service stack before beginning to delete
    the build stack, we would destroy IAM roles we needed to finish deleting
    the service stack.

--- scripts/deleteBuildInfra.sh
+++ scripts/deleteBuildInfra.sh
@@ -12,13 +12,18 @@ aws ecr batch-delete-image \
       sed 's/sha256:/imageDigest=sha256:/g' | \
       sed 's/image/ image/g')
 
-echo deleting associated service stack $ESM_APP_NAME-$ESM_API_SERVICE_NAME-$ESM_ENV
+echo deleting associated service stack $ESM_API_SERVICE_STACK_NAME
 aws cloudformation delete-stack \
-  --stack-name $ESM_API_SERVICE_STACK_NAME \
-  --role-arn arn:aws:iam::054393190750:role/CloudFormationReaper
+  --stack-name $ESM_API_SERVICE_STACK_NAME
+aws cloudformation describe-stacks \
+  --stack-name $ESM_API_SERVICE_STACK_NAME
+while [ 0 -lt $? ]
+do
+  aws cloudformation describe-stacks \
+    --stack-name $ESM_API_SERVICE_STACK_NAME
+done
 
 echo deleting build stack $ESM_API_BUILD_STACK_NAME
 aws cloudformation delete-stack \
-  --stack-name $ESM_API_BUILD_STACK_NAME \
-  --role-arn arn:aws:iam::054393190750:role/CloudFormationReaper
+  --stack-name $ESM_API_BUILD_STACK_NAME
 
