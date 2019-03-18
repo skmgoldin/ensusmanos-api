f7f62f2c6c327202f3a4cddd1ec4759765aa6c02
Mike Goldin <mike.goldin@protonmail.ch>
2019-03-17 21:45:19 -0400

    Avoid collisions in top-level stack export names

--- scripts/deleteBuildInfra.sh
+++ scripts/deleteBuildInfra.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ecrName=$(aws cloudformation list-exports | \
-  jq -cj '.[] | .[] | select(contains({Name: "ensusmanos-EcrRepositoryName"})) | .Value')
+  jq -cj --arg searchValue $ESM_APP_NAME:$ESM_API_SERVICE_NAME:$ESM_ENV:EcrRepositoryName '.[] | .[] | select(contains({Name: $searchValue})) | .Value')
 
 echo deleting ecr $ecrName
 aws ecr batch-delete-image \
@@ -15,9 +15,10 @@ aws ecr batch-delete-image \
 echo deleting associated service stack $ESM_APP_NAME-$ESM_API_SERVICE_NAME-$ESM_ENV
 aws cloudformation delete-stack \
   --stack-name $ESM_API_SERVICE_STACK_NAME \
+  --role-arn arn:aws:iam::054393190750:role/CloudFormationReaper
 
 echo deleting build stack $ESM_API_BUILD_STACK_NAME
 aws cloudformation delete-stack \
   --stack-name $ESM_API_BUILD_STACK_NAME \
-
+  --role-arn arn:aws:iam::054393190750:role/CloudFormationReaper
 
